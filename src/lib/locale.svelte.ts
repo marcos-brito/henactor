e96837eba2bcbe521a1eb69026bc772da1e7a1d5
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-16 20:56:49 -0300

    feat: load locales at the start

---
+++ src/lib/locale.svelte.ts
@@ -0,0 +1,12 @@
+import { register, init, getLocaleFromNavigator, dictionary as _dictionary, locale as _locale } from 'svelte-i18n';
+import { fromStore, } from 'svelte/store';
+
+export const locale = fromStore(_locale);
+export const dictionary = fromStore(_dictionary);
+
+register('en', () => import('./locale/en.json'));
+register('pt-BR', () => import('./locale/pt-BR.json'));
+init({
+    fallbackLocale: 'en',
+    initialLocale: getLocaleFromNavigator(),
+});
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import "$lib/locale.svelte";
+    import "../app.css";
+    import { config } from "$lib/config.svelte";
+    import { waitLocale } from "svelte-i18n";
+    import { locale } from "$lib/locale.svelte";
+    import type { Snippet } from "svelte";
+    let {
+        children,
+    }: {
+        children: Snippet;
+    } = $props();
+
+    $effect(() => {
+        waitLocale();
+        locale.current = config.options.lang;
+    });
+</script>
+
