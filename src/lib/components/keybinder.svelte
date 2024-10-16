4b4ea8e9d591a747abe7d5b7833f11fd11adfdd0
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-16 20:12:34 -0300

    refactor: use component instead of class to set keybinds

---
+++ src/lib/components/keybinder.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+    import type { Command } from "../../bindings";
+    import { config } from "$lib/config.svelte";
+
+    let {
+        actions,
+        container = $bindable(),
+        children,
+    }: {
+        actions?: Partial<Record<Command, () => void>>;
+        container?: HTMLElement;
+        children: Snippet;
+    } = $props();
+
+    let holding = $state<Array<String>>([]);
+    let fullBinding = $derived(holding.join("+"));
+
+    function handleKeyDown(e: KeyboardEvent): void {
+        holding.push(e.key);
+
+        for (const [cmd, keys] of Object.entries(config.options.keybindings)) {
+            if (keys.includes(fullBinding) && actions) {
+                const action = actions[cmd as Command];
+                if (action) action();
+            }
+        }
+    }
+
+    function handleKeyUp(e: KeyboardEvent): void {
+        holding = holding.filter((key) => key != e.key);
+    }
+</script>
+
+<div bind:this={container} tabindex="-1" onkeyup={handleKeyUp} onkeydown={handleKeyDown}>
+    {@render children()}
+</div>
