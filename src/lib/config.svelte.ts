1eea9a62c888238fd7e536c4817e3e444b6f2d4d
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-17 00:51:05 -0300

    fix: listen only to the config change listener

--- src/lib/config.svelte.ts
+++ src/lib/config.svelte.ts
@@ -8,15 +8,18 @@ async function createConfig() {
     let currentTheme = $derived(themes.find((t) => t.name == options.current_theme))
 
     if (options.auto_reload) {
-        await commands.watch(configPath);
-        events.watchEvent.listen(async () => {
-            options = await commands.loadConfig(configPath);
-            themes = await commands.findThemes(configPath);
+        const watcherId = await commands.watch(configPath);
+        events.watchEvent.listen(async (e) => {
+            if (e.payload.includes(watcherId.toString())) {
+                options = await commands.loadConfig(configPath);
+                themes = await commands.findThemes(configPath);
+            }
         });
     }
 
 
     return {
+        configPath,
         get options() { return options },
         get themes() { return themes },
         get currentTheme() { return currentTheme }
