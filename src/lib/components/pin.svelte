23168ff8f3d7e7adccdf7ef8d1dddb0fb03ccf58
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-18 16:57:53 -0300

    feat: show the pins in a sidebar

---
+++ src/lib/components/pin.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { type Pin } from "$lib/bindings";
+    import Icon from "$lib/components/icon/icon.svelte";
+
+    let { pin }: { pin: Pin } = $props();
+</script>
+
+<li>
+    <a href={`${pin.target}`}>
+        {#if pin.icon}
+            <Icon icon={pin.icon} size={40} />
+        {/if}
+        {pin.alias}
+    </a>
+</li>
---
+++ src/lib/components/sidebar.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { config } from "$lib/config.svelte";
+    import Pin from "$lib/components/pin.svelte";
+    import { _ } from "svelte-i18n";
+</script>
+<aside id="sidebar" tabindex="-1" bind:this={element}>
+    <ul class="menu">
+        {#if config.options.title}
+            <h1 class="title mb-8 text-3xl">{config.options.title}</h1>
+        {/if}
+        <li class="menu-title">{$_("pins")}</li>
+        {#each config.pins as pin}
+            <Pin {pin} />
+        {/each}
+    </ul>
+</aside>
