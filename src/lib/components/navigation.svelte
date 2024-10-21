cb08164e595dde2320051924aa51b1767bfbfc3b
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-21 19:42:05 -0300

    feat: add component to move across elements with keyboard

---
+++ src/lib/components/navigation.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import Keybinder from "$lib/components/keybinder.svelte";
+
+    let {
+        parent,
+        childrens = $bindable(),
+        orientation,
+    }: {
+        parent: HTMLElement;
+        childrens: Array<HTMLElement>;
+        orientation: "Vertical" | "Horizontal" | "Both";
+    } = $props();
+
+    let selected = $state(0);
+    let selectedChild = $derived(childrens.at(selected));
+
+    function selectPrevious(): void {
+        if (selected == 0) return;
+        selected--;
+        selectedChild?.focus();
+    }
+
+    function selectNext(): void {
+        if (selected == childrens.length - 1) return;
+        selected++;
+        selectedChild?.focus();
+    }
+
+    function handleFocus(): void {
+        selected = 0;
+        selectedChild?.focus();
+    }
+
+    parent.addEventListener("focus", handleFocus);
+</script>
+
+{#if orientation == "Vertical"}
+    <Keybinder element={parent} actions={{ Down: selectNext, Up: selectPrevious }}></Keybinder>
+{/if}
+{#if orientation == "Horizontal"}
+    <Keybinder element={parent} actions={{ Left: selectPrevious, Right: selectNext }}></Keybinder>
+{/if}
