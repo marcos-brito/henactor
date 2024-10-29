c755aed5a1686e5f82c2041f33d04b07ef0e1c9d
Marcos Brito <marcos.brito0507@gmail.com>
2024-10-29 07:37:52 -0300

    feat: add some margins to the context menu

--- src/lib/components/context-menu/context-menu.svelte
+++ src/lib/components/context-menu/context-menu.svelte
@@ -20,8 +20,8 @@
         if (menu && isActive) {
             const windowWidth = window.innerWidth;
             const windowHeight = window.innerHeight;
-            if (x + menu.offsetWidth > windowWidth) x = windowWidth - menu.offsetWidth;
-            if (y + menu.offsetHeight > windowHeight) y = windowHeight - menu.offsetHeight;
+            if (x + menu.offsetWidth > windowWidth) x = windowWidth - menu.offsetWidth - 30;
+            if (y + menu.offsetHeight > windowHeight) y = windowHeight - menu.offsetHeight - 30;
             menu.focus();
         }
     });
@@ -34,6 +34,7 @@
 
     function handleClick(e: MouseEvent): void {
         e.preventDefault();
+        e.stopPropagation();
         openAt(e.clientX, e.clientY);
     }
 
@@ -52,7 +53,7 @@
         bind:this={menu}
         use:clickOutside={() => (isActive = false)}
         onclick={() => (isActive = false)}
-        class="menu absolute z-10 rounded-box bg-base-200"
+        class="menu absolute z-10 w-48 rounded-box bg-base-200"
         style:top={`${y}px`}
         style:left={`${x}px`}
     >
