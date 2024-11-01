6e6bfae343ac6e6dc7a7e28a5b128be58ce25f00
Marcos Brito <marcos.brito0507@gmail.com>
2024-11-01 18:12:45 -0300

    refactor: make caching code more functional

--- src-tauri/src/cache.rs
+++ src-tauri/src/cache.rs
@@ -34,30 +34,22 @@ pub fn download_or_find(cache_dir: PathBuf, url: String) -> Result<PathBuf> {
 fn extract_ext(res: &Response) -> String {
     res.headers()
         .get("content-type")
-        .and_then(|content_type| {
-            let content_type = content_type.to_str().ok()?;
-
-            Some(
-                mime_guess::get_mime_extensions_str(content_type)?
-                    .first()?
-                    .to_string(),
-            )
-        })
-        .unwrap_or("".to_string())
+        .and_then(|header| header.to_str().ok())
+        .and_then(|content_type| mime_guess::get_mime_extensions_str(content_type))
+        .and_then(|guesses| guesses.first())
+        .unwrap_or(&"")
+        .to_string()
 }
 
 fn find_file(cache_dir: &PathBuf, hash: &str) -> Option<PathBuf> {
     fs::read_dir(cache_dir).ok()?.find_map(|entry| {
         let entry = entry.ok()?;
 
-        if entry
+        entry
             .path()
             .file_stem()
             .is_some_and(|stem| stem.to_string_lossy().to_string() == hash)
-        {
-            return Some(entry.path());
-        }
-        None
+            .then_some(entry.path())
     })
 }
 
