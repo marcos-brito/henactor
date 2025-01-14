e4e5db94ede835328c5fcee6a5810c5bd282f9e4
Marcos Brito <marcos.brito0507@gmail.com>
2025-01-14 16:53:33 -0300

    refactor: allow components pass open as a prop to modal

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -5,32 +5,40 @@
 
     let {
         children,
+        open = $bindable(),
         trigger,
         ...props
     }: {
-        trigger: Snippet<[HTMLDialogElement]>;
+        trigger?: Snippet<[HTMLDialogElement]>;
         children: Snippet;
         [key: string]: any;
     } = $props();
 
     let dialog = $state<HTMLDialogElement>();
+
+    $effect(() => {
+        if (dialog) open ? dialog.showModal() : dialog.close();
+    });
 </script>
 
-{#if dialog}
+{#if dialog && trigger}
     {@render trigger(dialog)}
 {/if}
 <dialog class="modal" bind:this={dialog}>
     <div class={`modal-box p-8 ${props.class}`}>
+        {@render children()}
         <form method="dialog">
-            <button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">
+            <button
+                onclick={() => (open = false)}
+                class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2"
+            >
                 <IconWithFallback iconName="x">
                     <XIcon size="20" />
                 </IconWithFallback>
             </button>
         </form>
-        {@render children()}
     </div>
     <form method="dialog" class="modal-backdrop">
-        <button>close</button>
+        <button onclick={() => (open = false)}>close</button>
     </form>
 </dialog>
