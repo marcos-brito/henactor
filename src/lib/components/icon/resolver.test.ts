51ac6a57664291c9df1dfc4eaa9c97870599b643
Marcos Brito <marcos.brito0507@gmail.com>
2025-02-24 15:48:57 -0300

    feat: write some tests that won't run for the icon resolver

--- src/lib/components/icon/resolver.test.ts
+++ src/lib/components/icon/resolver.test.ts
@@ -1,5 +1,23 @@
-import { expect, describe, it, } from "vitest";
-import { IconType, identifyIcon } from "./resolver";
+import { expect, describe, it, afterEach, beforeAll, beforeEach } from "vitest";
+import { mockIPC, clearMocks } from "@tauri-apps/api/mocks";
+import { IconType, identifyIcon, resolve } from "./resolver";
+// @ts-ignore
+import { randomFillSync } from "crypto";
+
+afterEach(() => {
+    clearMocks();
+});
+
+beforeAll(() => {
+    Object.defineProperty(window, "crypto", {
+        value: {
+            // @ts-ignore
+            getRandomValues: (buffer) => {
+                return randomFillSync(buffer);
+            },
+        },
+    });
+});
 
 describe("idenfityIcon", () => {
     it("should return Icon for iconify strings", () => {
@@ -26,5 +44,43 @@ describe("idenfityIcon", () => {
         for (const icon of icons) {
             expect(IconType[identifyIcon(icon)]).toBe(IconType[IconType.Url]);
         }
+    });
+});
+
+describe("resolve", () => {
+    beforeEach(async () => {
+        // @ts-ignore
+        mockIPC((cmd, args) => {
+            switch (cmd) {
+                case "download_or_find":
+                    // @ts-ignore
+                    return `${args.cacheDir}/${Math.random()}`;
+            }
+        });
+    });
+
+    it("should return cached URL download is true", async () => {
+        const uri = await resolve("http://joe.com/doe.png", "", true);
+        expect(uri).toMatch(/^asset:\/\//);
+    })
+
+    it("should return cached Icon when download is true", async () => {
+        const uri = await resolve("some:icon", "", true);
+        expect(uri).toMatch(/^asset:\/\//);
+    })
+
+    it("should resolve relative paths to basePath", async () => {
+        const uri = await resolve("./some/icon.png", "/base/path", true);
+        expect(uri).toMatch(`asset:///base/path/some/icon.png`);
+    });
+
+    it("should return iconify url when download is false", async () => {
+        const uri = await resolve("some:icon", "", false);
+        expect(uri).toMatch(`https://api.iconify.design/some/icon.svg`);
+    });
+
+    it("should return the url itself when download is false", async () => {
+        const uri = await resolve("https://some.com/icon.svg", "", false);
+        expect(uri).toMatch("https://some.com/icon.svg");
     });
 });
