0716347590b9a1ca66bd00df365c427276bb6187
Marcos Brito <marcos.brito0507@gmail.com>
2025-02-26 13:35:12 -0300

    feat: create an interface for commands

---
+++ src/lib/commands/index.ts
@@ -0,0 +1,62 @@
+
+export interface Command<T> {
+    name: string;
+    desc: string;
+    keybinds: Array<string>;
+    execute(arg: T): Promise<void>;
+    undo?(arg: T): Promise<void>;
+}
+
+export class Register {
+    private commands = new Map<string, Command<any>>;
+
+    public register(cmd: Command<any>): Register {
+        this.commands.set(cmd.name, cmd);
+        for (const keybind of cmd.keybinds) {
+            this.commands.set(keybind, cmd);
+        }
+
+        return this;
+    }
+
+    public find(query: string): Command<any> | undefined {
+        return this.commands.get(query);
+    }
+
+    public all(): Array<Command<any>> {
+        return Array.from(this.commands.values());
+    }
+}
+
+type HistoryEntry = {
+    cmd: Command<any>,
+    arg: any,
+}
+
+export class Executor {
+    private undoHistory: Array<HistoryEntry> = [];
+    private redoHistory: Array<HistoryEntry> = [];
+
+    public async do<T>(cmd: Command<T>, arg: T): Promise<void> {
+        await cmd.execute(arg);
+        this.undoHistory.push({ cmd, arg });
+    }
+
+    public async undo() {
+        const entry = this.undoHistory.pop();
+
+        if (entry?.cmd?.undo) {
+            await entry.cmd.undo(entry.arg);
+            this.redoHistory.push(entry);
+        }
+    }
+
+    public async redo() {
+        const entry = this.redoHistory.pop();
+
+        if (entry?.cmd?.undo) {
+            await entry.cmd.undo(entry.arg);
+            this.undoHistory.push(entry);
+        }
+    }
+}
