8838f56d78f3ee3ed14e12f331f07e84d405294f
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-05 11:31:29 -0300

    fixup! refactor: use one global key press listener

--- src/lib/components/keylistener.svelte
+++ src/lib/components/keylistener.svelte
@@ -3,17 +3,16 @@
 
     let holding = $state<Array<String>>([]);
 
-    async function handleKeyDown(e: KeyboardEvent): Promise<void> {
+    async function maybeExecute(e: KeyboardEvent): Promise<void> {
         if (!e.repeat) holding.push(e.key);
         const command = commandRegister.find(holding.join("+"));
         if (command && (await command.canExecute())) await command.execute();
     }
 
-    function handleKeyUp(): void {
-        // filtering makes it buggy and pop makes it wrong. Needs some work.
+    function clearLastKey(): void {
         holding.pop();
     }
 
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
+    window.addEventListener("keydown", maybeExecute);
+    window.addEventListener("keyup", clearLastKey);
 </script>
