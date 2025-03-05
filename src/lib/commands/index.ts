2a4bdef5495e55abc7c39d2106c149197a8bdd1c
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-05 11:04:48 -0300

    refactor: make commands take no args to execute

--- src/lib/commands/index.ts
+++ src/lib/commands/index.ts
@@ -1,24 +1,19 @@
-import { Delete, Create, type CreateArgs, Rename } from "./fs"
-import { NewTab } from "./tabs"
-import { type Command as CommandKind } from "$lib/bindings";
 
-export {
-    Delete, Create, type CreateArgs, Rename, NewTab
-}
-
-export interface Command<T> {
-    kind: CommandKind
+export interface Command {
     name: string;
     desc: string;
     keybinds: Array<string>;
-    execute(arg: T): Promise<void>;
+    canExecute(): Promise<boolean>;
+    execute(): Promise<void>;
 }
 
 export class Register {
-    private commands = new Map<string, Command<any>>;
+    private commands = new Map<string, Command>();
 
     public register(cmd: Command<any>): Register {
         this.commands.set(cmd.kind, cmd);
+    public register(cmd: Command): Register {
+        this.commands.set(cmd.identifier, cmd);
         for (const keybind of cmd.keybinds) {
             this.commands.set(keybind, cmd);
         }
@@ -26,14 +21,11 @@ export class Register {
         return this;
     }
 
-    public find(query: string): Command<any> | undefined {
+    public find(query: string): Command | undefined {
         return this.commands.get(query);
     }
 
-    public all(): Array<Command<any>> {
-        return Array.from(this.commands.values());
-    }
-}
-
+    public all(): Array<Command> {
+        return Array.from(new Set(this.commands.values()));
     }
 }
