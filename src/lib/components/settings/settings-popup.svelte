962d0498b8fbca8cc02d1d6c974721a72561dbb5
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-05 10:53:56 -0300

    feat: allow modals to be open programmatically

--- src/lib/components/settings/settings-popup.svelte
+++ src/lib/components/settings/settings-popup.svelte
@@ -2,7 +2,7 @@
     import type { Snippet } from "svelte";
     import Modal from "../modal.svelte";
     import SettingsField from "./settings-field.svelte";
-    import { i18n } from "$lib";
+    import { i18n, modalManager } from "$lib";
 
     let {
         name,
@@ -13,15 +13,15 @@
         desc: string;
         children: Snippet;
     } = $props();
+
+    const modalName = `settings.${name}`;
 </script>
 
 <SettingsField {name} {desc}>
-    <Modal>
-        {#snippet trigger(dialog: HTMLDialogElement)}
-            <button class="btn btn-sm" onclick={() => dialog.showModal()}
-                >{i18n.t("configure", { ns: "settings" })}</button
-            >
-        {/snippet}
+    <button class="btn btn-sm" type="button" onclick={() => modalManager.show(modalName)}
+        >{i18n.t("configure", { ns: "settings" })}</button
+    >
+    <Modal name={modalName} {modalManager}>
         <div class="mb-2">
             <h1 class="text-lg font-bold">{name}</h1>
             <p class="opacity-70">{desc}</p>
