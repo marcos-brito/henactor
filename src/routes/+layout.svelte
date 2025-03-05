bab04494f90bdba2f8da2b2c81d686983dd6f254
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-05 11:26:34 -0300

    refactor: use a single component to mount modals

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,8 @@
     import { type Snippet } from "svelte";
     import Tabs from "$lib/components/tab/tabs.svelte";
     import { Toaster, toast } from "svelte-sonner";
-    import { configManager } from "$lib";
+    import { configManager, commandRegister, modalManager } from "$lib";
+    import Modals from "$lib/components/modals.svelte";
     import Keylistener from "$lib/components/keylistener.svelte";
 
     let {
@@ -32,6 +33,7 @@
     {/if}
 </svelte:head>
 <Keylistener />
+<Modals register={commandRegister} {modalManager} />
 <Toaster
     position="top-right"
     visibleToasts={1}
