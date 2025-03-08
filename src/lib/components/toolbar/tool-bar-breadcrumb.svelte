83cb2a63ac3dae1e78fd5b14d6bb17d3b537e791
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-08 04:40:13 -0300

    feat: open dir picker when clicking on the breadcrumb

--- src/lib/components/toolbar/tool-bar-breadcrumb.svelte
+++ src/lib/components/toolbar/tool-bar-breadcrumb.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { path as pathApi } from "@tauri-apps/api";
-    import { clickOutside } from "$lib/utils";
+    import { modalManager } from "$lib";
 
     let {
         path = $bindable(),
@@ -8,9 +8,7 @@
         path: string;
     } = $props();
 
-    let editMode = $state(false);
     let ref: HTMLElement;
-    let input = $state<HTMLElement>();
     let parts = $derived(path.split(pathApi.sep()).slice(1));
 
     function buildPathUpTo(idx: number) {
@@ -22,34 +20,21 @@
             ref.scrollTo(ref.scrollWidth, 0);
         }
     });
-
-    $effect(() => {
-        if (input) input.focus();
-    });
 </script>
 
 <div bind:this={ref} class="breadcrumbs text-sm">
-    {#if editMode}
-        <input
-            bind:this={input}
-            use:clickOutside={() => (editMode = false)}
-            class="input input-sm input-ghost"
-            bind:value={path}
-        />
-    {:else}
-        <ul ondblclick={() => (editMode = true)}>
+    <ul ondblclick={() => modalManager.show("pallete:goto")}>
+        <li>
+            <button class="btn btn-ghost btn-sm" onclick={() => (path = pathApi.sep())}>
+                {pathApi.sep()}
+            </button>
+        </li>
+        {#each parts as part, i}
             <li>
-                <button class="btn btn-ghost btn-sm" onclick={() => (path = pathApi.sep())}>
-                    {pathApi.sep()}
+                <button class="btn btn-ghost btn-sm" onclick={() => (path = buildPathUpTo(i))}>
+                    {part}
                 </button>
             </li>
-            {#each parts as part, i}
-                <li>
-                    <button class="btn btn-ghost btn-sm" onclick={() => (path = buildPathUpTo(i))}>
-                        {part}
-                    </button>
-                </li>
-            {/each}
-        </ul>
-    {/if}
+        {/each}
+    </ul>
 </div>
