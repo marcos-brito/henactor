6896f111d011cadd53aa91beb845a72ffff338a0
Marcos Brito <marcos.brito0507@gmail.com>
2025-03-14 15:59:39 -0300

    feat: create an explorer for empty stuff

--- src/lib/components/explorer/explorer-empty.svelte
+++ src/lib/components/explorer/explorer-empty.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
     import { BirdIcon } from "lucide-svelte";
     import IconWithFallback from "$lib/components/icon/icon-with-fallback.svelte";
-    import ExplorerActions from "./explorer-actions.svelte";
+    import ExplorerBase from "./explorer-base.svelte";
     import { i18n } from "$lib";
+    import { GridNavigator } from "../navigator";
 
-    let ref = $state<HTMLElement>();
+    let { path = $bindable() }: { path: string } = $props();
+
+    const navigator = new GridNavigator(0, 0);
 </script>
 
-{#if ref}
-    <ExplorerActions trigger={ref} />
-{/if}
-<div bind:this={ref} class="flex h-full w-full flex-col justify-center gap-4">
-    <IconWithFallback size={160} iconName="empty_dir">
-        <BirdIcon strokeWidth={0.5} size={160} />
+<ExplorerBase {navigator} bind:path items={[]} container={undefined} entries={[]} selected={[]}>
+    <div class="flex h-full w-full flex-col justify-center gap-4">
+        <IconWithFallback size={160} iconName="empty_dir">
+            <BirdIcon strokeWidth={0.5} size={160} />
+        </IconWithFallback>
         <h1 class="text-xl font-bold">{i18n.t("emptyDir", { ns: "ui" })}</h1>
-    </IconWithFallback>
-</div>
+    </div>
+</ExplorerBase>
