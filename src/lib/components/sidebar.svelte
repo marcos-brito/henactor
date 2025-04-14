65d55073354524b8f704d670a4dd51db6743c6df
Marcos Brito <marcos.brito0507@gmail.com>
2025-04-14 13:32:56 -0300

    feat: use localized string for sidebar items

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Pin from "$lib/components/pin.svelte";
-    import { configManager, i18n, modalManager } from "$lib";
+    import { configManager, i18n, modalManager, taskManager } from "$lib";
     import IconWithFallback from "./icon/icon-with-fallback.svelte";
-    import { SettingsIcon, TerminalIcon } from "lucide-svelte";
+    import { HistoryIcon, ListTodoIcon, SettingsIcon, TerminalIcon } from "lucide-svelte";
 </script>
 
 <aside>
@@ -15,14 +15,39 @@
             <Pin {pin} />
         {/each}
         <details open>
-            <summary class="menu-title">Tools</summary>
+            <summary class="menu-title">{i18n.t("sidebar.tools.title", { ns: "ui" })}</summary>
             <ul>
                 <li>
                     <button onclick={() => modalManager.show("pallete:commands")}>
                         <IconWithFallback size={16} iconName="commands">
                             <TerminalIcon size="16" />
                         </IconWithFallback>
-                        Commands
+                        {i18n.t("sidebar.tools.items.commands", { ns: "ui" })}
+                    </button>
+                </li>
+                <li>
+                    <button onclick={() => modalManager.show("settings")}>
+                        <IconWithFallback size={16} iconName="settings">
+                            <HistoryIcon size="16" />
+                        </IconWithFallback>
+                        {i18n.t("sidebar.tools.items.history", { ns: "ui" })}
+                    </button>
+                </li>
+                <li>
+                    <button onclick={() => modalManager.show("tasks")}>
+                        <IconWithFallback size={16} iconName="settings">
+                            <ListTodoIcon size="16" />
+                        </IconWithFallback>
+                        <div class="indicator">
+                            {#if Array.from(taskManager.tasks.values()).length > 0}
+                                <span class="indicator-item indicator-middle badge badge-xs"
+                                    >{Array.from(taskManager.tasks.values()).length}</span
+                                >
+                            {/if}
+                            <p class="mr-4">
+                                {i18n.t("sidebar.tools.items.tasks", { ns: "ui" })}
+                            </p>
+                        </div>
                     </button>
                 </li>
                 <li>
@@ -30,8 +55,8 @@
                         <IconWithFallback size={16} iconName="settings">
                             <SettingsIcon size="16" />
                         </IconWithFallback>
-                        Settings</button
-                    >
+                        {i18n.t("sidebar.tools.items.settings", { ns: "ui" })}
+                    </button>
                 </li>
             </ul>
         </details>
