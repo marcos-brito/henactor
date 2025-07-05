2402d80545f7a3ac80d0d82c8a107fde7100c189
Marcos Brito <marcos.brito0507@gmail.com>
2025-07-05 18:50:36 -0300

    feat: use TaskHandle for search command

--- src-tauri/src/fs/query/mod.rs
+++ src-tauri/src/fs/query/mod.rs
@@ -1,28 +1,15 @@
-use crate::{Result, TaskKillEvent};
-use anyhow::Context;
-use serde::Serialize;
-use specta::Type;
 use crate::fs::entry::Entry;
+use crate::task::{Message, TaskHandle};
+use crate::Result;
+use log::warn;
+pub use parser::Rule;
 use std::path::PathBuf;
-use std::sync::{
-    atomic::{AtomicBool, Ordering},
-    Arc,
-};
-use tauri::{ipc::Channel, AppHandle, Listener};
-use tauri_specta::Event;
+use tauri::{ipc::Channel, AppHandle};
 use walkdir::WalkDir;
 
 mod ast;
 mod eval;
 mod parser;
-
-#[derive(Clone, Serialize, Type)]
-#[serde(tag = "event", content = "data")]
-pub enum SearchEvent {
-    Started(u32),
-    Progress(Entry),
-    Finished,
-}
 
 #[tauri::command]
 #[specta::specta]
@@ -44,40 +31,24 @@ pub async fn search(
     app: AppHandle,
     path: PathBuf,
     query: String,
-    on_event: Channel<SearchEvent>,
+    on_event: Channel<Message<Entry>>,
 ) -> Result<()> {
+    let mut handle = TaskHandle::new(app, on_event);
+    handle.start()?;
+
     let expr = parser::parse(&query).map(|pairs| parser::convert(pairs))?;
-    let task_id = rand::random::<u32>();
-    let should_stop = Arc::new(AtomicBool::new(false));
-    let should_stop_c = Arc::clone(&should_stop);
-
-    on_event
-        .send(SearchEvent::Started(task_id.clone()))
-        .with_context(|| "Failed to emit start event")?;
-
-    TaskKillEvent::listen(&app, move |event| {
-        if event.payload.0 == task_id {
-            should_stop_c.store(true, Ordering::Relaxed);
-        }
-    });
-
-    for entry in WalkDir::new(&path).into_iter().filter_map(|e| {
-        e.ok()
-            .and_then(|e| Entry::try_from(e.path().to_path_buf()).ok())
-            .and_then(|e| eval::eval(&e, expr.clone()).ok().map(|r| r.then_some(e)))
-    }) {
-        if should_stop.load(Ordering::Relaxed) {
-            break;
-        }
-
-        if let Some(e) = entry {
-            let _ = on_event.send(SearchEvent::Progress(e));
+    for entry in WalkDir::new(&path) {
+        match entry.map(|e| Entry::from(e.path().to_path_buf())) {
+            Ok(entry) => {
+                if eval::eval(&entry, expr.clone())? {
+                    handle.send(entry)?;
+                }
+            }
+            Err(e) => warn!("skipping entry from {}: {}", path.display(), e),
         }
     }
 
-    on_event
-        .send(SearchEvent::Finished)
-        .with_context(|| "Failed to emit finished event")?;
+    handle.finish()?;
 
     Ok(())
 }
